e804bb1e30d1c2a7baf9dae901a90b53ac8b89c4
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2022-12-14 11:34:05 -0600

    Code from week 2 class

---
+++ MarkAnthonyG/Week2Code/index.js
@@ -0,0 +1,104 @@
+console.log("Hello!");
+
+
+let studentsInCohortNov2022 = ["Mark Anthony", "MauriceA", "Chuck C", "MikeT", "ChrisK", "Summer", "Amy","Stacy", "Jared", "Barrack",];
+
+let allStudents = new Array(1000);
+
+// for (let i = 0; i < names.length; i++) { 
+//     console.log(names[i]);
+// }
+
+let numOfHellos = 0;
+
+console.log(allStudents);
+
+// for (let x = 0; x < names.length; x++) {
+
+// }
+
+for (let x = 0; x < studentsInCohortNov2022.length; x++) {
+    allStudents.push(studentsInCohortNov2022[x]);
+}
+
+console.log(allStudents);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// I need to know the age
+
+// const max = 1;
+// const min = 0;
+// let uber = Math.floor(Math.random() * (max - min + 1) + min);
+
+// const age = prompt("How old are you?");
+
+// console.log(`You are ${age} years old.`);
+
+// if (age >= 21) {
+//     const numOfDrinks = prompt("How many drinks have you had?");
+//     switch (numOfDrinks) {
+//         case "0":
+//             alert(`Hey, you should probably have another drinkn`);
+//             break;
+//         case "1":
+//             alert(`Hey you've had ${numOfDrinks} drinks! Have 1 more!`);
+//             break;
+//         case "2": 
+//             alert(`Hey you've had ${numOfDrinks} drinks! Have 1 more!`);
+//             break;
+//         case "3": 
+//             const selfDriving = prompt('Are you driving yourself home?');
+
+//             if (selfDriving === "yes") {
+//                 alert("You should beat the buzz! And drinks some water. That will be $3.99");
+//             } else if (!uber) {
+//                 console.log("uber", uber);
+//                 alert("You need to walk home, hope you live close!");
+//             } else {
+//                 console.log("There was no uber!");
+//                 alert("Enjoy your Uber!");
+//             }
+
+//             //0 for no, 1 for yes
+//             alert(`Hey you've had ${numOfDrinks}! Have 1 more!`);
+//             break;
+//         default: 
+//             alert("You have had way to much to drink, we're calling you an uber");
+//     }
+
+//     // alert("You are able to drink!");
+
+
+// } else {
+//     alert("Leave the bar! You're undage!");
+
+    
+// }
+
+
+
+// # of drinks that they've had
+
+// age 
+
+// # of hours it's been since their last drink
+
+// # of ubers located near to our bar
+
+
+//alert us what should happen in any given scenario
+
+// walk home!
+
